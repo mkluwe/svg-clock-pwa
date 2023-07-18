b0a293513a12b6f941e53ebf9519c32f80a853a0
Matthias Kluwe <mkluwe@gmail.com>
2023-07-18 16:18:55 +0200

    try to acquire wakelock

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let wakeLock = null;
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request();
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
+await requestWakeLock();
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/svg-clock-pwa/sw.js', { scope: '/svg-clock-pwa/' });
 }
